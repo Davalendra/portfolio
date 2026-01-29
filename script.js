b9b7f7b5be1bce364399f6c70691b3983c0d717b
Davalendra <davalendrakuruba@gmail.com>
2026-01-29 14:11:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function toggleMode() {
+const heroText = document.querySelector(".hero h1");
+
+heroText.style.opacity = 0;
+
+window.addEventListener("load", () => {
+  heroText.style.transition = "2s";
+  heroText.style.opacity = 1;
+});
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-}
+});
